f20a60d4afed7f450f9bb5b48fdafb7a83e63340
meghaarora <maggi1129@hotmail.com>
2023-09-03 14:39:31 +0530

    Initial commit

---
+++ MoviesListingDemoApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MoviesListingDemoApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
